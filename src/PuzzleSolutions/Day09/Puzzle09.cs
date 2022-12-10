cc80667303cd4fc4bbd80bfb32b38a84e1615c45
MarcusOtter <github@otterstrom.dev>
2022-12-11 00:16:36 +0100

    Day 9 part 2 + refactor

--- src/PuzzleSolutions/Day09/Puzzle09.cs
+++ src/PuzzleSolutions/Day09/Puzzle09.cs
@@ -9,54 +9,73 @@ public class Puzzle09 : IPuzzleSolver
 		public bool IsAdjacentOrEqual(Position other) => Math.Abs(X - other.X) <= 1 && Math.Abs(Y - other.Y) <= 1;
 	}
 	
-	private readonly HashSet<Position> _visited = new() { new Position(0, 0) };
-	
 	public string SolveFirstPart(string[] input)
 	{
-		var head = new Position(0, 0);
-		var tail = new Position(0, 0);
-		
+		return GetTailVisitedCount(input, 2).ToString();
+	}
+
+	public string SolveSecondPart(string[] input)
+	{
+		return GetTailVisitedCount(input, 10).ToString();
+	}
+
+	private static int GetTailVisitedCount(IEnumerable<string> input, int ropeLength)
+	{
+		var visited = new HashSet<Position> { new(0, 0) };
+		var ropeKnots = Enumerable.Repeat(new Position(0, 0), ropeLength).ToArray();
+
 		foreach (var instruction in input)
 		{
 			var distance = int.Parse(instruction[2..]);
 			for (var i = 0; i < distance; i++)
 			{
-				head = instruction[0] switch
-				{
-					'R' => head with { X = head.X + 1 },
-					'L' => head with { X = head.X - 1 },
-					'U' => head with { Y = head.Y + 1 },
-					'D' => head with { Y = head.Y - 1 },
-					_ => throw new ArgumentOutOfRangeException()
-				};
-
-				if (tail.IsAdjacentOrEqual(head)) continue;
-			
-				tail = MoveTowardsHead(head, tail);
-				_visited.Add(tail);
+				ropeKnots[0] = GetHeadPosition(ropeKnots[0], instruction[0]);
+				var tailPosition = UpdateRopeTail(ropeKnots);
+				visited.Add(tailPosition);
 			}
 		}
 
-		return _visited.Count.ToString();
+		return visited.Count;
 	}
 
-	public string SolveSecondPart(string[] input)
+	private static Position UpdateRopeTail(IList<Position> ropeKnots)
 	{
-		return "";
+		for (var j = 1; j < ropeKnots.Count; j++)
+		{
+			var targetKnot = ropeKnots[j - 1];
+			var knot = ropeKnots[j];
+			
+			if (knot.IsAdjacentOrEqual(targetKnot)) break;
+			ropeKnots[j] = MoveTowardsPosition(knot, targetKnot);
+		}
+
+		return ropeKnots[^1]; // Return tail position
 	}
 
-	private static Position MoveTowardsHead(Position head, Position tail)
+	private static Position GetHeadPosition(Position head, char direction)
 	{
-		var xSign = Math.Sign(head.X - tail.X);
-		var ySign = Math.Sign(head.Y - tail.Y);
-		var x = tail.X + xSign;
-		var y = tail.Y + ySign;
-	
+		return direction switch
+		{
+			'R' => head with { X = head.X + 1 },
+			'L' => head with { X = head.X - 1 },
+			'U' => head with { Y = head.Y + 1 },
+			'D' => head with { Y = head.Y - 1 },
+			_ => throw new ArgumentOutOfRangeException()
+		};
+	}
+
+	private static Position MoveTowardsPosition(Position source, Position target)
+	{
+		var xSign = Math.Sign(target.X - source.X);
+		var ySign = Math.Sign(target.Y - source.Y);
+		var x = source.X + xSign;
+		var y = source.Y + ySign;
+
 		// Prefer diagonal moves
 		if (xSign != 0 && ySign != 0) return new Position(x, y);
-		if (xSign != 0) return tail with { X = x };
-		if (ySign != 0) return tail with { Y = y };
+		if (xSign != 0) return source with { X = x };
+		if (ySign != 0) return source with { Y = y };
 
-		return tail;
+		return source;
 	}
 }
